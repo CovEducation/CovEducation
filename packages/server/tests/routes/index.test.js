70703201892a576c802ccc5f111713d1d75b61e3
mi-yu <m.yu8088@gmail.com>
2020-06-22 23:00:22 -0500

    Run lint in CI step
    - also ran autofix on all files

--- packages/server/tests/routes/index.test.js
+++ packages/server/tests/routes/index.test.js
@@ -1,10 +1,10 @@
-const request = require("supertest");
-const app = require("../../app");
+const request = require('supertest');
+const app = require('../../app');
 
-describe("Test Index Routes", () => {
-    test("/heartbeat", async () => {
-        const response = await request(app).get("/heartbeat");
-        expect(response.statusCode).toBe(200);
-        expect(response.text).toBe("coved api is working");
-    });
+describe('Test Index Routes', () => {
+  test('/heartbeat', async () => {
+    const response = await request(app).get('/heartbeat');
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toBe('coved api is working');
+  });
 });
