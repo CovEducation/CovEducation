7d8c7f9af22f53f888a77d064192f9f7e1c79c99
Ojasw U <ojaswupadhyay@gmail.com>
2020-07-01 12:19:28 -0700

    A wild goose chase for dependencies. Fixed.

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from './components/Button';
+import VTabs from './components/Tabs';
 
 function App() {
   var [apiStatus, setApiStatus] = useState(null);
@@ -15,31 +16,32 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button theme="default" size="md" onClick={() => alert('hello')}>
-          Click me
-        </Button>
-        <Button theme="accent" basic onClick={() => alert('hello')}>
-          Click me
-        </Button>
-
-        <Button size="sm" onClick={() => alert('hello')}>
-          Click me
-        </Button>
-        {apiStatus}
-      </header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      {apiStatus}
+      <br />
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+      <Button theme="default" size="md" onClick={() => alert('hello')}>
+        Click me
+      </Button>
+      <Button theme="accent" basic onClick={() => alert('hello')}>
+        Click me
+      </Button>
+      <Button size="sm" onClick={() => alert('hello')}>
+        Click me
+      </Button>
+      <br />
+      <VTabs texts={['1', '2', '3', 'counting is fun']} values={[1, 2, 3, 4]}/>
+      <br />
     </div>
   );
 }
