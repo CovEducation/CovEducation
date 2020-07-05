0b7261097a5705020702677a0d93e2f28ac92bea
Ojasw U <ojaswupadhyay@gmail.com>
2020-07-05 12:34:07 -0700

    added dropdown component

---
+++ packages/client/src/components/Dropdown/index.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import './index.scss';
+// import onClickOutside from 'react-onclickoutside';
+
+function Dropdown({ title, items, multiSelect = false }) {
+    const [open, setOpen] = useState(false);
+    const [selection, setSelection] = useState([]);
+    const toggle = () => setOpen(!open);
+    Dropdown.handleClickOutside = () => setOpen(false);
+
+    function handleOnClick(item) {
+        if (!selection.some(current => current.id === item.id)) {
+            if (!multiSelect) {
+                setSelection([item]);
+            } else if (multiSelect) {
+                setSelection([...selection, item]);
+            }
+        } else {
+            let selectionAfterRemoval = selection;
+            selectionAfterRemoval = selectionAfterRemoval.filter(
+                current => current.id !== item.id
+            );
+            setSelection([...selectionAfterRemoval]);
+        }
+    }
+
+    function isItemInSelection(item) {
+        return selection.some(current => current.id === item.id);
+
+    }
+
+    return (
+        <div className="dd-wrapper">
+            <div
+                tabIndex={0}
+                className="dd-header"
+                role="button"
+                onKeyPress={() => toggle(!open)}
+                onClick={() => toggle(!open)}
+            >
+                <div className="dd-header__title">
+                    <p className="dd-header__title--bold">{title}</p>
+                </div>
+                <div className="dd-header__action">
+                    <p>{open ? 'Close' : 'Open'}</p>
+                </div>
+            </div>
+            {open && (
+                <ul className="dd-list">
+                    {items.map(item => (
+                        <li className="dd-list-item" key={item.id}>
+                            <button type="button" onClick={() => handleOnClick(item)}>
+                                <span>{item.value}</span>
+                                <span>{isItemInSelection(item) && 'Selected'}</span>
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
+}
+
+// const clickOutsideConfig = {
+//     handleClickOutside: () => Dropdown.handleClickOutside,
+// };
+
+const items = [
+    {
+        id: 1,
+        value: 'Elementary',
+    },
+    {
+        id: 2,
+        value: 'Middle',
+    },
+    {
+        id: 3,
+        value: 'High',
+    },
+];
+
+export default Dropdown;
