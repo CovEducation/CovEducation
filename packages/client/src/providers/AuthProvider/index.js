273220e29e8f6075c941e4e2011c42f0f0aae81b
mi-yu <m.yu8088@gmail.com>
2020-07-10 22:08:56 -0500

    Move firebase config to .env files

--- packages/client/src/providers/AuthProvider/index.js
+++ packages/client/src/providers/AuthProvider/index.js
@@ -3,13 +3,13 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyCSyjSnzA5mp8Y3ro904vg_wxMsG3ELsek',
-    authDomain: 'coved-test.firebaseapp.com',
-    databaseURL: 'https://coved-test.firebaseio.com',
-    projectId: 'coved-test',
-    storageBucket: 'coved-test.appspot.com',
-    messagingSenderId: '779651794617',
-    appId: '1:779651794617:web:8b9bf2f99882b58d337d68'
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
