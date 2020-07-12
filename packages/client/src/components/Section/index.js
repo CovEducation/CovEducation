7b62313cbeb41f75603a1c6898c226549df2ea4c
Erik Yu <madergaser10@gmail.com>
2020-07-12 17:52:11 -0500

    delete uneeded code

--- packages/client/src/components/Section/index.js
+++ packages/client/src/components/Section/index.js
@@ -7,18 +7,12 @@ import { COLORS } from '../../constants';
 const useStyles = makeStyles((theme) => ({
     root: {
         backgroundColor: props => props.backgroundColor in COLORS ? COLORS[props.backgroundColor] : COLORS['white'],
-        margin: props => props.m,
-        padding: props => props.p,
-        mx: props => props.mx,
     },
 }));
 
 export default function Section(props = {
     children: '',
     backgroundColor: 'white',
-    m: 0,
-    p: 0,
-    mx: 'auto',
 }) {
     const { children, backgroundColor, m, p, mx, ...otherProps } = props;
     const classes = useStyles(props);
