d559808cba158fae9fc1e52a8daf20a35407ecbd
Kevin Chen <kchen3568@gmail.com>
2020-07-15 20:26:05 -0500

    General layout of mentor card

--- packages/client/src/pages/FindAMentor/Mentor.js
+++ packages/client/src/pages/FindAMentor/Mentor.js
@@ -1,22 +1,40 @@
-import React, { useState } from "react";
+import React from "react";
+import './mentor.scss'
 
 // Displays the picture, name, and major of a mentor.
-const Mentor = (mentor) => {
+const Mentor = ({ mentor }) => {
     if (!validateMentorData(mentor)) return (<></>);
+
+    const relevantInformation = [
+        ["subjects", "Subjects"],
+        ["location", "Location"],
+        ["grade_levels_to_mentor", "Grades"],
+        ["timezone", "Timezone"],
+        ["languages_spoken", "Languages"],
+        ["email", "Email"]
+    ];
     
     return (
-        <div className="container">
-            <div className="row">
-                <img width="100%" height="100%"src={mentor.profilePicture}/>
+        <div className="mentor-card-container">
+            <div className="mentor-card">
+                <img src={mentor.avatar} alt="Profile picture" />
+                <div>
+                    <h3>{mentor.name}</h3>
+                    <h4>{mentor.school}</h4>
+                    <h4>{mentor.major}</h4>
+                </div>
             </div>
-            <div className="row">
-                {mentor.name}
-                {mentor.major}
-                {mentor.subjects}
+            {mentor.bio !== null &&
+                <div>
+                    <p>Bio</p>
+                    <p>{mentor.bio}</p>
+                </div>
+            }
+            <div className="mentor-information">
+                {relevantInformation.map(field => displayField(field, mentor))}
             </div>
         </div>
     )
-    
 }
 
 const validateMentorData = (mentor) => {
@@ -24,3 +42,18 @@ const validateMentorData = (mentor) => {
     // TODO(johancc) - Implement a more through validation if needed.
     return true;
 }
+
+const displayField = (field, mentor) => {
+    let mentorInfo = mentor[field[0]];
+    if (Array.isArray(mentorInfo)) {
+        mentorInfo = mentorInfo.join(", ");
+    }
+    return (
+        <div>
+            <p>{field[1]}</p>
+            <p>{mentorInfo}</p>
+        </div>
+    )
+};
+
+export default Mentor;
--- packages/client/src/pages/Profile/index.js
+++ packages/client/src/pages/Profile/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import Mentor from '../FindAMentor/Mentor';
 
 const ProfilePage = () => {
-  return (
-    <div>Profile page</div>
-  )
+    const mentor = {
+        "_id" : "5ee15af7e790bd0a065b70f9",
+        "subjects" : ["Physics", "Math"],
+        "languages_spoken" : ["English", "Spanish"],
+        "grade_levels_to_mentor" : ["Middle School", "High School"],
+        "mentees" : [],
+        "tags" : [],
+        "firebase_uid" : "sNku1aj94rZz0fLYwFwW2qhvdFI3",
+        "name" : "Maria Mentor",
+        "email" : "mariamentor@coved.org",
+        "timezone" : "Central Daylight Time - Chicago (GMT-5)",
+        "bio" : "Hello! My name is Maria Mentor and I am a Rising Senior at UCLA studying History",
+        "major" : "Computer Science",
+        "last_request" : "2020-06-10T22:13:11.769Z",
+        "public" : true,
+        "__v" : 0
+    };
+
+    return (
+        // <div>Profile page</div>
+        <Mentor mentor={mentor}/>
+    )
 }
 
 export default ProfilePage;
