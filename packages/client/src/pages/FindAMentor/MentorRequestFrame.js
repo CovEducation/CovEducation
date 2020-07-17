7f59be75e5657146e07ee41d0ecb058dbbd645b7
Kevin Chen <kchen3568@gmail.com>
2020-07-16 23:56:31 -0500

    wip mentor column

--- packages/client/src/pages/FindAMentor/MentorRequestFrame.js
+++ packages/client/src/pages/FindAMentor/MentorRequestFrame.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
+import MentorProfile from './MentorProfile';
 
 // Shows more detailed mentor information and allows parents to request mentors.
 const MentorRequestFrame = (mentor) => {
-    // TODO(johanc) - Implement.
+    // TODO(johanc) - Implement.\
+    return <MentorProfile />;
 }
+
+export default MentorRequestFrame;
