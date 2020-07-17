700b912f6e9fa90e8ec9e1f8080025e8d7b6127d
Dheekshita Kumar <dheekshita@Dheekshitas-MacBook-Pro-4.local>
2020-07-17 19:57:33 -0400

    add more sections

--- packages/client/src/pages/Home/index.js
+++ packages/client/src/pages/Home/index.js
@@ -42,6 +42,20 @@ const HomepageSubtitle = styled.p`
   color: ${COLORS.darkblue};
 `;
 
+const HomepageQuote = styled.p`
+  font-size: min(3vw, 24px);
+  font-weight: 300;
+  font-family:${FONTS.font2};
+  color: ${COLORS.darkblue};
+  padding-left: 10vw;
+  padding-right: 10vw;
+  h3 {
+    font-size: min(3.5vw, 36px);
+    font-weight: 400;
+  }
+`;
+
+
 const HomePage = () => {
   const { t } = useTranslation();
   return (
@@ -62,9 +76,15 @@ const HomePage = () => {
         <h2>{t('home.howCovEdWorks')}</h2>
       </Section>
       <Section p="100px">
-        <h2>{t('home.findTutors')}</h2>
+      Statistics
       </Section>
       <Section backgroundColor='lightblue' p="100px">
+      <HomepageQuote><h3>" I am so grateful for how much my CovEd mentor has taught me! Where would I have been without him, I truly don't know! Thanks CovEducation! "</h3><br /><p>- John Smith</p></HomepageQuote>
+      </Section>
+      <Section p="100px">
+        <h2>{t('home.findTutors')}</h2>
+      </Section>
+      <Section backgroundColor='lightorange' p="100px">
         <h2>{t('home.questions')}</h2>
       </Section>
     </HomeWrapper>
