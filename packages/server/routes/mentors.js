8b5e6cb46ba1ddabf6906eb968240f70617da655
Johan Cervantes <jccamacho19992@gmail.com>
2020-07-23 07:14:05 -0500

    Fixes firebase initialization issue

--- packages/server/routes/mentors.js
+++ packages/server/routes/mentors.js
@@ -5,12 +5,6 @@ const router = express.Router();
 // Firebase boilerplate.
 const admin = require('firebase-admin');
 
-const serviceAccount = require('../serviceAccount.json');
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
 const db = admin.firestore();
 
 const mentorsRef = db.collection('mentors');
