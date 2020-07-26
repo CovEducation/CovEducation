20d846015f464c034ddd9182921f50429dcc8f9f
Nicholas Salinas <nsalinas@mit.edu>
2020-07-26 11:20:06 -0700

    resolved issue with merge w/ master

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -5,13 +5,9 @@ import {
 import './App.css';
 import HomePage from './pages/Home';
 import ProfilePage from './pages/Profile';
-<<<<<<< HEAD
 import SignUpPage from './pages/SignUp';
-=======
 import SignInPage from './pages/SignIn';
-import SignUpPage from './pages/SignUp';
 import Signin from './components/SignIn';
->>>>>>> master
 import { AuthProvider } from './providers/AuthProvider';
 
 function App() {
@@ -26,23 +22,17 @@ function App() {
           <Route path="/profile">
             <ProfilePage />
           </Route>
-<<<<<<< HEAD
           <Route path="/signup">
             <SignUpPage />
           </Route>
-=======
           <Route path="/signin">
             <SignInPage />
             {/* for testing and firebase stuffs :) */}
-          </Route>
-          <Route path="/signup">
-            <SignUpPage />
           </Route>
           <Route path="/forgot-password">
             <br/>
           </Route>
           <Route path="/sign-in" component={Signin} />
->>>>>>> master
         </Switch>
       </Router>
     </AuthProvider>
