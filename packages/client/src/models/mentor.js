acf329471a78d8e494951169d11a8e33a5bc8313
Ojasw U <ojaswupadhyay@gmail.com>
2020-08-03 00:25:55 -0700

    added fields

--- packages/client/src/models/mentor.js
+++ packages/client/src/models/mentor.js
@@ -8,10 +8,17 @@ const MentorConverter = {
         return {
             name: mentor.name,
             email: mentor.email,
+            pronouns: mentor.pronouns,
+            college: mentor.college,
+            avatar: mentor.avatar,
+            bio: mentor.bio,
+            major: mentor.major,
             timezone: mentor.timezone,
+            // what is this for?
             about: mentor.about,
             subjects: mentor.subjects,
-            tags: mentor.tags
+            tags: mentor.tags,
+            gradeLevels: mentor.gradeLevels
         };
     },
 
@@ -20,10 +27,17 @@ const MentorConverter = {
         return new Mentor(
             data.name,
             data.email,
+            data.pronouns,
+            data.college,
+            data.avatar,
+            data.bio,
+            data.major,
             data.timezone,
+            // what is this for?
             data.about,
             data.subjects,
-            data.tags
+            data.tags,
+            data.gradeLevels
         );
     }
 };
@@ -35,21 +49,45 @@ export default class Mentor {
      * Use this class to create and modify mentors in firebase.
      * @constructor
      */
-    constructor(name, email, timezone, about, subjects, tags) {
+    constructor(
+        name,
+        email,
+        pronouns,
+        college,
+        avatar,
+        bio,
+        major,
+        timezone,
+        // what is this for?
+        about,
+        subjects,
+        tags,
+        gradeLevels
+    ) {
         this.id = undefined;
         this.name = name;
         this.email = email;
+        this.pronouns = pronouns;
+        this.college = college;
+        this.avatar = avatar;
+        this.bio = bio;
+        this.major = major;
         this.timezone = timezone;
+        // what is this for?
         this.about = about;
         this.subjects = subjects;
         this.tags = tags;
+        this.gradeLevels = gradeLevels;
 
         this.validate();
     }
 
+    /**
+     *
+     */
     validate() {
-        // TODO use Yup for object validation; this validation just needs
-        // to run in development
+        // TODO use Yup for object validation; this validation just needs to run in development
+        // TODO add other validation as required
         if (!this.name) throw Error('Mentor must have a name');
         if (!this.email) throw Error('Mentor must have an email');
         if (!this.timezone) throw Error('Mentor must have timezone');
@@ -76,12 +114,13 @@ export default class Mentor {
 
     /**
      * Publishes the current mentor instance to firebase
-     * @param {firebase.auth.UserCredential} the firebase user object
+     * @param {firebase.auth.UserCredential} user object from firebase
      *
      * @return {Promise<void>} a promise indicating successful creation.
      */
     create(user) {
         this.validate();
+        // uid is an unresolved variable
         this.id = user.uid; // Saving the id in the object so update can be called.
         return MentorCollectionRef.doc(this.id)
             .withConverter(MentorConverter)
@@ -90,7 +129,7 @@ export default class Mentor {
 
     /**
      * Reads the Mentor object from firebase.
-     * @param {firebase.auth.UserCredential} the firebase auth uid of the mentor
+     * @param {firebase.auth.UserCredential}  user has firebase auth uid of the mentor
      *
      * @return {Promise<Mentor>} the mentor with the corresponding uid
      */
