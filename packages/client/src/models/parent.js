32bfa13fa80c144567a7962e1e439e1b104ccf10
Ojasw U <ojaswupadhyay@gmail.com>
2020-08-06 19:12:08 -0700

    fixed small error

--- packages/client/src/models/parent.js
+++ packages/client/src/models/parent.js
@@ -24,7 +24,7 @@ const ParentConverter = {
             pronouns: parent.pronouns,
             avatar: parent.avatar,
             timezone: parent.timezone,
-            students: students
+            students: parent.students
         };
     },
 
