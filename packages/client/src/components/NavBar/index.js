247461b18dd30bbdde16599664005c332b407b66
mi-yu <m.yu8088@gmail.com>
2020-08-24 16:38:04 -0500

    Fix navbar dropdown

--- packages/client/src/components/NavBar/index.js
+++ packages/client/src/components/NavBar/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import IconButton from '@material-ui/core/IconButton';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -8,14 +8,9 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import PropTypes from 'prop-types';
-import Modal from '../Modal';
-import Wizard from '../Wizard';
 import Button from '../Button';
 import styled from 'styled-components';
 import { FONTS, COLORS } from '../../constants';
-
-
-const Wiz_content = ['page1', <Button>oh boi</Button>, 'page3']
 
 const TextThemes = {
   fontSize: {
@@ -51,74 +46,71 @@ const LinkStyled = styled(Link)`
   }
 `
 
+const UserLinkWrapper = styled.div`
+  margin-left: auto;
+`;
+
 export default function NavBar(props)  {
-
-  const [anchorEl, setAnchorEl] = useState(null);
-  const open = Boolean(anchorEl);
-  const handleMenu = (event) => {
-    setAnchorEl(event.currentTarget);
+  const [menuDropdownAnchor, setMenuDropdownAnchor] = useState(null);
+  const handleMenuDropdownClick = (event) => {
+    setMenuDropdownAnchor(event.currentTarget);
+    event.stopPropagation();
   };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const [anchorEl2, setAnchorEl2] = useState(null);
-  const open2 = Boolean(anchorEl2);
-  const handleMenu2 = (event) => {
-    setAnchorEl2(event.currentTarget);
-  };
-  const handleClose2 = () => {
-    setAnchorEl2(null);
+  const handleMenuDropdownClose = () => {
+    setMenuDropdownAnchor(null);
   };
 
   let userLinks;
   if (true) {
     userLinks = (
       <>
-        <LinkStyled to='/login' ver='default'>Login</LinkStyled>
-        <div/>
-        <Modal title="Sign Up" trigger={<Button theme='accent' size='sm'> Sign Up </Button>}> <Wizard content={Wiz_content} /> </Modal>
+        <LinkStyled to='/signin' ver='default'>Login</LinkStyled>
+        <LinkStyled to='/signup' ver='default'>
+          <Button theme='accent' size='sm'>
+            Sign Up
+          </Button>
+        </LinkStyled>
       </>
     );
   } else {
-    userLinks = (
-      <>
-        <IconButton
-          aria-label="account of current user"
-          aria-controls="menu-navbar"
-          aria-haspopup="true"
-          onClick={handleMenu}
-          color="inherit"
-          onMouseOver={handleMenu}
-        >
-          <AccountCircleIcon/>
-          <div style={{ padding:'10px' }}/>
-          <LinkStyled ver='default' style={{ color: COLORS.blue }}>
-            {'Tim Beaver'}
-          </LinkStyled>
-        </IconButton>
-        <Menu
-          id="menu-navbar"
-          anchorEl={anchorEl}
-          getContentAnchorEl={null}
-          anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'center',
-          }}
-          keepMounted
-          transformOrigin={{
-            vertical: 'top',
-            horizontal: 'center',
-          }}
-          open={open}
-          onClose={handleClose}
-          MenuListProps={{ onMouseLeave: handleClose }}
-        >
-          <MenuItem component={Link} to="/profile" style={{ fontSize: TextThemes.fontSize['default'] }}>Dashboard</MenuItem>
-          <MenuItem style={{ color: 'red', fontSize: TextThemes.fontSize['default'] }}>Sign Out</MenuItem>
-        </Menu>
-      </>
-    )
+    // userLinks = (
+    //   <>
+    //     <IconButton
+    //       aria-label="account of current user"
+    //       aria-controls="menu-navbar"
+    //       aria-haspopup="true"
+    //       onClick={handleMenu}
+    //       color="inherit"
+    //       onMouseOver={handleMenu}
+    //     >
+    //       <AccountCircleIcon/>
+    //       <div style={{ padding:'10px' }}/>
+    //       <LinkStyled ver='default' style={{ color: COLORS.blue }}>
+    //         {'Tim Beaver'}
+    //       </LinkStyled>
+    //     </IconButton>
+    //     <Menu
+    //       id="menu-navbar"
+    //       anchorEl={anchorEl}
+    //       getContentAnchorEl={null}
+    //       anchorOrigin={{
+    //         vertical: 'bottom',
+    //         horizontal: 'center',
+    //       }}
+    //       keepMounted
+    //       transformOrigin={{
+    //         vertical: 'top',
+    //         horizontal: 'center',
+    //       }}
+    //       open={open}
+    //       onClose={handleClose}
+    //       MenuListProps={{ onMouseLeave: handleClose }}
+    //     >
+    //       <MenuItem component={Link} to="/profile" style={{ fontSize: TextThemes.fontSize['default'] }}>Dashboard</MenuItem>
+    //       <MenuItem style={{ color: 'red', fontSize: TextThemes.fontSize['default'] }}>Sign Out</MenuItem>
+    //     </Menu>
+    //   </>
+    // )
   }
 
   return (
@@ -127,9 +119,10 @@ export default function NavBar(props)  {
         <Toolbar>
           <Grid>
             <LinkStyled to='/' ver='lg'>CovEd</LinkStyled>
+            <LinkStyled to='#' onClick={handleMenuDropdownClick}>How It Works</LinkStyled>
             <Menu
               id="howitworks"
-              anchorEl={anchorEl2}
+              anchorEl={menuDropdownAnchor}
               getContentAnchorEl={null}
               anchorOrigin={{
                 vertical: 'bottom',
@@ -140,21 +133,22 @@ export default function NavBar(props)  {
                 vertical: 'top',
                 horizontal: 'center',
               }}
-              open={open2}
-              onClose={handleClose2}
-              MenuListProps={{ onMouseLeave: handleClose2 }}
+              open={Boolean(menuDropdownAnchor)}
+              onClose={handleMenuDropdownClose}
+              MenuListProps={{ onMouseLeave: handleMenuDropdownClose }}
             >
               <MenuItem component={LinkStyled} to="/parents">For Parents</MenuItem>
               <MenuItem component={LinkStyled} to="/mentors">For Mentors</MenuItem>
             </Menu>
             {props.links.map(link =>(
-              <LinkStyled to={link.link} ver='default'>
+              <LinkStyled key={link.link} to={link.link} ver='default'>
                 {link.title}
               </LinkStyled>
             ))}
-            </Grid>
-          <div style={{marginLeft: 'auto'}}/>
-          {userLinks}
+          </Grid>
+          <UserLinkWrapper>
+            {userLinks}
+          </UserLinkWrapper>
         </Toolbar>
       </AppBar>
     </>
