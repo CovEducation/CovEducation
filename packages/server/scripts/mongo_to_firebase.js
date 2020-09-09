8b065708defe31f1c8dcf66646a69458775292f0
Johan Cervantes <jccamacho19992@gmail.com>
2020-09-08 22:06:49 -0400

    migration skeleton code

---
+++ packages/server/scripts/mongo_to_firebase.js
@@ -0,0 +1,61 @@
+// Firebase boilerplate.
+const firebase = require('firebase-admin');
+const fs = require('fs');
+const serviceAccount = require('../service_account.json');
+
+const {
+  createUser,
+} = require('../db/users');
+
+const BACKUP_PATH = './backup_september.json'; // Later.
+firebase.initializeApp({
+  credential: firebase.credential.cert(serviceAccount),
+});
+
+// firebase setup
+// the google service account file path should be in FIREBASE_CREDENTIALS
+// the database name should be in FIREBASE_URL
+const firebaseCredentials = JSON.parse(fs.readFileSync(process.env.FIREBASE_CREDENTIALS || './service_account.json'));
+if (!firebaseCredentials) {
+  throw new Error('Cannot find google service account credentials');
+}
+firebase.initializeApp({
+  credential: firebase.credential.cert(firebaseCredentials),
+  databaseURL: process.env.FIREBASE_URL,
+});
+
+const addOldMentorToNewSite = (oldMentor) => {
+  const adaptedMentor = {
+    email: oldMentor.email,
+    name: oldMentor.name,
+    timezone: oldMentor.timezone,
+    bio: oldMentor.bio,
+    major: oldMentor.major,
+    subjects: oldMentor.subjects,
+    avatar: '/images/stockProfile.png',
+    gradeLevels: oldMentor.grade_levels_to_mentor,
+    pronouns: '',
+    college: '',
+    phone: '',
+    role: 'MENTOR',
+  };
+  return createUser(oldMentor.uid, adaptedMentor);
+};
+
+const CURRENT_MENTORS = JSON.parse(fs.readFileSync(BACKUP_PATH));
+
+CURRENT_MENTORS.map((mentor) => addOldMentorToNewSite(mentor));
+
+const addOldParentToNewSite = (oldParent) => {
+  const adaptedParent = {
+    // All the parent data
+    name: oldParent.name, // etc.
+    students: [
+      {
+        name: oldParent.studentName,
+        // etc.
+      },
+    ],
+  };
+  return createUser(oldParent.uid, adaptedParent);
+};
