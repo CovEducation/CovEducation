ede09d35ac8e5c63db9d7aa5590f8f4d92a30bd4
Ojasw U <ojaswupadhyay@gmail.com>
2020-09-10 23:27:04 -0700

    create empty test

---
+++ packages/server/tests/scripts/mongo_to_firebase.test.js
@@ -0,0 +1,10 @@
+const firebase = require('firebase-admin');
+const userDb = require('../../db/users');
+
+jest.mock('firebase-admin');
+
+describe('Mongo to Firebase', () => {
+    test('getUser', async () => {
+        // add this later
+    });
+});
