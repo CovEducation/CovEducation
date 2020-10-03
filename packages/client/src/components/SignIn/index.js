9aec1d3d4dcca7b263c1a0329b1d8c048df91b25
Sanjay Yepuri <sanjay.yepuri@gmail.com>
2020-10-03 17:00:16 -0500

    refactored sign in page

--- packages/client/src/components/SignIn/index.js
+++ packages/client/src/components/SignIn/index.js
@@ -1,17 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import IconButton from '@material-ui/core/IconButton';
 import { Visibility, VisibilityOff } from '@material-ui/icons';
 import Grid from '@material-ui/core/Grid';
 import styled from 'styled-components';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Container from '@material-ui/core/Container';
 import Text from '../../components/TextBox';
-import Checkbox from '@material-ui/core/Checkbox';
-import useAuth from '../../providers/AuthProvider'
+import useAuth from '../../providers/AuthProvider';
 import Button from '../Button';
 import UncontrolledAlert from '../Notification/UncontrolledAlert';
-import { FormControl } from '@material-ui/core';
 import * as Yup from 'yup';
 import { useFormik } from 'formik';
 
@@ -31,17 +28,6 @@ const Notification = styled(UncontrolledAlert)`
     width: 33ch;
     border: 0.0625rem solid transparent;
 `
-const PassForget = styled.p`
-    text-align: right;
-    font-size: 13px;
-    padding-top: 10px;
-    color: #7f7d7d;
-    margin: 0;
-
-    a {
-        color: #00568C;
-    }
-`
 const Title = styled.h1`
     text-align: center;
 `
@@ -50,24 +36,52 @@ const handleMouseDownPassword = (event) => {
     event.preventDefault();
 };
 
-const validateUserFields = (email, password) => {
-    const longEnough = email.length > 0 && password.length > 5;
-    const validEmail = email.includes('@') && email.includes('.');
-    return validEmail && longEnough;
-}
-
 const SigninSchema = Yup.object().shape({
     email: Yup.string().email('Please enter a valid email')
-            .required('Please enter your email.'),
+        .required('Please enter your email.'),
     password: Yup.string().required('Please enter your password.')
 });
 
 const Signin = () => {
     const { signin } = useAuth();
-    const [formError, setFormError] = useState(false);
-    const [remember, setRemember] = useState(false);
     const [serverError, setServerError] = useState(false);
     const [showPassword, setShowPassword] = useState(false);
+    const [submittedOnce, setSubmittedOnce] = useState(false);
+
+
+    const handleClickShowPassword = () => {
+        setShowPassword(!showPassword);
+    }
+
+    const handleSubmit = (values) => {
+        setSubmittedOnce(true);
+        // We don't have to return anything since the
+        // Notification component will alert the user.
+        signin(values.email, values.password)
+            .then((resp) => {
+                // Redirect.
+                alert('Success! Redirecting to sign in...');
+            })
+            .catch((err) => {
+                console.log(err);
+                setServerError(true);
+            });
+    }
+
+
+    const ShowNotifications = () => {
+        if (serverError) {
+            return (
+                <Notification>
+                    <span>
+                        Incorrect email/password.
+                    </span>
+                </Notification>
+            )
+        } else {
+            return <> </>
+        }
+    }
 
     const formik = useFormik({
         initialValues: {
@@ -75,36 +89,9 @@ const Signin = () => {
             password: '',
         },
         validationSchema: SigninSchema,
-        onSubmit: values => console.log(JSON.stringify(values))
+        onSubmit: handleSubmit
     });
 
-    const handleClickShowPassword = () => {
-        setShowPassword(!showPassword);
-    }
-
-
-    const ShowNotifications = () => {
-        if (!formError && !serverError) return <></>;
-
-        if (formError) {
-            return (
-                <Notification>
-                    <span>
-                        The email and/or password are in the wrong format. Please try again.
-                    </span>
-                </Notification>
-            )
-        }
-        if (serverError) {
-            return (
-                <Notification>
-                    <span>
-                        Wrong email and password.
-                    </span>
-                </Notification>
-            )
-        }
-    }
 
     return (
         <Container component="form" maxWidth="xs" onSubmit={formik.handleSubmit}>
@@ -143,34 +130,17 @@ const Signin = () => {
                             </InputAdornment>
                     }}
                 />
-                {/* <Grid item xs={12} align='left'>
-                    <FormControl>
-                        <FormControlLabel
-                            style={{
-                                paddingLeft: '13px'
-                            }}
-                            control={
-                                <Checkbox
-                                    id="remember"
-                                    onChange={formik.handleChange}
-                                    value={formik.values.remember}
-                                    name="remember"
-                                    color="primary"
-                                />
-                            }
-                            label="Remember Me"
-                        />
-                    </FormControl>
-                </Grid> */}
                 <Grid item xs={12} align='center'>
                     <Button
                         type="submit"
                         theme="default"
                         size="md"
-                        >
+                    >
                         Login
                     </Button>
                 </Grid>
+                {submittedOnce && <ShowNotifications />}
+
                 <Grid item xs={12} align='right'>
                     <a href="/forgot-password">Forgot password?</a>
                 </Grid>
--- packages/client/src/pages/SignIn/index.js
+++ packages/client/src/pages/SignIn/index.js
@@ -1,17 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import AppBar from '@material-ui/core/AppBar';
-import Grid from '@material-ui/core/Grid';
 import SignIn from '../../components/SignIn';
 
 const SignInPageWrapper = styled.div`
     display: flex;
     min-height: 75vh;
     padding-top: 3em;
-`;
-
-const SignInWizardWrapper = styled.div`
-    padding: 1em;
 `;
 
 const SignInPage = () => {
