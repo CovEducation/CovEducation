5e2410d0ec9ad0973281ad068215bd93aa2fe6b8
sadhanalolla <lolla.sadhana@gmail.com>
2021-01-21 20:50:55 -0500

    edits

--- packages/client/src/pages/VideoConference/index.js
+++ packages/client/src/pages/VideoConference/index.js
@@ -3,33 +3,40 @@ import React, { useState } from 'react'
 import { Jutsu } from 'react-jutsu'
 
 const VideoConferencePage = () => {
-  const [room, setRoom] = useState('')
+  const [room, setRoom] = useState('test')
+  const [password, setPassword] = useState('password')
+  const [userPassword, setUserPassword] = useState(password)
+  const [mentor, setMentor] = useState('')
   const [name, setName] = useState('')
   const [call, setCall] = useState(false)
-  const [password, setPassword] = useState('')
 
   const handleClick = event => {
     event.preventDefault()
-    if (room && name) setCall(true)
+    if (room && name && mentor && userPassword == password) setCall(true)
   }
 
   return call ? (
     <Jutsu
       roomName={room}
       displayName={name}
-      password={password}
+      password={userPassword}
       onMeetingEnd={() => console.log('Meeting has ended')}
       loadingComponent={<p>loading ...</p>}
       errorComponent={<p>Oops, something went wrong</p>} />
   ) : (
       <form>
-        <input id='room' type='text' placeholder='Room' value={room} onChange={(e) => setRoom(e.target.value)} />
         <input id='name' type='text' placeholder='Name' value={name} onChange={(e) => setName(e.target.value)} />
-        <input id='password' type='text' placeholder='Password (optional)' value={password} onChange={(e) => setPassword(e.target.value)} />
+        <select id='mentor' placeholder='Mentor Name' value={mentor} onChange={(e) => setMentor(e.target.value)}>
+          <option value="default">Select One</option>
+          <option value="volvo">Volvo</option>
+          <option value="saab">Saab</option>
+          <option value="fiat">Fiat</option>
+          <option value="audi">Audi</option>
+        </select>
         <button onClick={handleClick} type='submit'>
           Start / Join
       </button>
-      </form>
+      </form >
     )
 }
 
