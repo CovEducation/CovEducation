3276898b701e869a39433ade3c45ab0bc266e6e1
sadhanalolla <lolla.sadhana@gmail.com>
2021-01-21 20:03:09 -0500

    added basic videoconferencing functionality

--- packages/client/src/pages/VideoConference/index.js
+++ packages/client/src/pages/VideoConference/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Jitsi from 'react-jitsi';
 
-
+/*hello testing*/
 const VideoConferencePage = () => {
 
   const [displayName, setDisplayName] = useState('')
