6526fe8607aceed90ac72157a7e6b829f643cc02
Hannah Kim <hannah@Hannahs-MacBook-Pro.local>
2021-01-23 15:03:49 -0500

    prettier form

--- packages/client/src/pages/VideoConference/index.js
+++ packages/client/src/pages/VideoConference/index.js
@@ -1,6 +1,40 @@
 import React, { useEffect, useState } from 'react'
 import { Jutsu } from 'react-jutsu'
 import { Auth, Db } from '../../providers/FirebaseProvider/index.js';
+import TextField from '@material-ui/core/TextField';
+import {FONTS} from '../../constants';
+import styled from 'styled-components';
+import { useTranslation } from 'react-i18next';
+import Button from '../../components/Button';
+import Container from '@material-ui/core/Container';
+
+const Wrapper = styled.div`
+  text-align: center;
+`;
+
+  const Title = styled.h1`
+  padding-top: 50px;
+  font-size: max(1.5vw, 32px);
+  font-weight: 400;
+  font-family: ${FONTS.font2};
+  left: calc(50% - 267px/2 + 0.5px)
+  top: 86px
+`;
+
+const Subtitle = styled.p`
+  font-size: max(1.1vw, 20px);
+  font-weight: 500;
+  font-style: normal;
+  font-family:${FONTS.font2};
+`;
+
+const Headings = styled.p`
+  font-style: normal;
+  font-size: max(1vw, 16px);
+  font-weight: 300;
+  font-family: ${FONTS.font2};
+  text-align: left;
+`;
 
 const VideoConferencePage = ({ user }) => {
   const [room, setRoom] = useState('CovEd-hfiaf8932hgWHIG3g028ha')
@@ -83,19 +117,29 @@ const VideoConferencePage = ({ user }) => {
       password={userPassword}
       onMeetingEnd={() => console.log('Meeting has ended')}
       loadingComponent={<p>loading ...</p>}
-      errorComponent={<p>Oops, something went wrong</p>} />
+      errorComponent={<p>Oops, something went wrong</p>} 
+      containerStyles={{ 
+        width: '90%', height: '100%' 
+      }}/>
   ) : (
-      <form>
-        <select id='mentor' placeholder='Mentor Name' onChange={updateRoomConfig}>
-          <option value="default">Select One</option>
-          {partnerList.map((name, index) => 
-            <option value = {index}> {name} </option>
-          )}
-        </select>
-        <button onClick={handleClick} type='submit'>
-          Start / Join
-      </button>
-      </form >
+    <Wrapper>
+        <Title>{"Start a Meeting"}</Title>
+        <Subtitle>{`Select your ${user.role=="MENTOR" ? "mentee" : "mentor"} from the dropdown`}</Subtitle>  <br />
+        <Container maxWidth="sm" >
+        <form>
+            <select id='mentor' placeholder='Mentor Name' onChange={updateRoomConfig}>
+              <option value="default">Select One</option>
+              {partnerList.map((name, index) => 
+                <option value = {index}> {name} </option>
+              )}
+            </select>
+            <br /> <br />
+            <Button theme="accent" size="md" onClick={handleClick} type='submit'>
+              Join
+            </Button>
+          </form >
+        </Container> <br /><br />
+    </Wrapper>
     )
 }
 
